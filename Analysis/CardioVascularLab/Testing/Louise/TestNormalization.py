730459f40603256cab1626a37cd0004925c2b260
Laura Gomez <gomez.lcgl@gmail.com>
2020-06-01 18:08:46 -0600

    Update TestNormalization
    Testing template using sample uniax data for normalization function

--- Analysis/CardioVascularLab/Testing/Louise/TestNormalization.py
+++ Analysis/CardioVascularLab/Testing/Louise/TestNormalization.py
@@ -1,23 +1,62 @@
-from TestingTemplate import Test
+#from TestingTemplate import Test
+import sys
+sys.path.append('../../ExVivo')
 
-def _normalizeData(self, xdata, ydata):
+import pandas as pd
+import numpy as np
+from matplotlib import pyplot as plt
+
+def calcStress(disp,force,dims):
+
+    #Cauchy Stress
+    stretch = (disp - disp[0])/dims1['length'] + 1
+    stress = force/(dims1['width']*dims1['thickness'])*stretch
+
+    return stress
+
+def calcStrain(disp, dims):
+
+    #Engineering Strain
+    disp = disp-disp[0]  # Zero the displacement for the first measurement
+    strain = disp/dims1['length'] # engineering strain
+
+    return strain
+
+def _normalizeData(xdata, ydata):
     '''
     This normalize 2 numpy arrays and normalize them
-    Some ideas: numpy.linalg.normalize(x) ... look up
-                x - min / (max-min)
-
     returns:
         numpy arrays
     '''
-    # not sure how to reference the correct arrays here
-    xdata = DataStructure._ProcessedData[0, :]
-    ydata = DataStructure._ProcessedData[:, 0]
 
     x_norm = xdata / np.linalg.norm(xdata)
     y_norm = ydata / np.linalg.norm(ydata)
 
-    # Normalize them
-    normArray1 = []
-    normArray2 = []
-
     return x_norm, y_norm
+
+# How the X_data Y_data will look once its in the data structure format
+    #xdata = DataStructure._ProcessedData[0, :]
+    #ydata = DataStructure._ProcessedData[:, 0]
+
+#Read in Sample Data
+df1 = pd.read_csv("NIH_BAV_Z2LC2_L1.csv", skiprows=[0,1,2,4], header = 'infer')
+df2 = pd.read_csv("NIH_BAV_Z2LC2_U1.csv", skiprows=[0,1,2,4], header = 'infer')
+dims1 = {'width':2.745,'thickness':1.75,'length':9.98}
+dims2 = {'width':2.87,'thickness':1.815,'length':8.67}
+
+#Calculate Stress Strain
+stress = calcStress(df1['Disp'],df1['Load'],dims1)
+strain = calcStrain(df1['Disp'],dims1)
+
+#Plot unprocessed stress v. strain
+plt.plot(strain,stress)
+plt.xlabel('Strain')
+plt.ylabel('Stress')
+plt.title('Test Data')
+
+#Normalize Data and Plot
+[strain_norm,stress_norm]=_normalizeData(stress,strain)
+plt.plot(strain,stress)
+plt.xlabel('strain_norm')
+plt.ylabel('stress_norm')
+plt.title('Test Data - Normalized')
